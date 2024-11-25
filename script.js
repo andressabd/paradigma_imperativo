21a1164b6795d626c98dff6866ba0dea845ca082
andressabd <anunes@cs.udf.edu.br>
2024-11-24 22:02:59 -0300

    Primeiro  commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Exemplo de animação ou interatividade (por exemplo, ao clicar no botão de contato)
+document.querySelector('.btn').addEventListener('click', function() {
+    alert('Obrigado por entrar em contato!');
+});
